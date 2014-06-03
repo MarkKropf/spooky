2144820a27203cb87b6e0801d407ceb14ad5d3e4
Mark Kropf <markkropf@gmail.com>
2014-06-03 01:02:27 -0700

    cleanup test

--- test/spooky-dmxSpec.js
+++ test/spooky-dmxSpec.js
@@ -4,10 +4,8 @@ var nconf = require('nconf');
 
   describe('emitter', function(){
     it('should emit an initialized', function(done){
-      var eventFired = false;
       nconf.file({ file: 'config.json'});
       spookyDmx.emitter.on('initialized',function() {
-        eventFired = true;
         assert(true);
         done();
       });
