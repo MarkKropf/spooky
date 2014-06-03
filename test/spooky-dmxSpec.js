9af5be02f4f7338ce739b8201403ef06b66fc07b
Mark Kropf <markkropf@gmail.com>
2014-06-03 14:29:12 -0700

    eliminator ed15 refactor

--- test/spooky-dmxSpec.js
+++ test/spooky-dmxSpec.js
@@ -2,7 +2,7 @@ var assert = require("assert");
 var spookyDmx = require("../lib/spooky-dmx.js");
 var nconf = require('nconf');
 
-  describe('emitter', function(){
+  describe('spookyDmx', function(){
     it('should emit an initialized', function(done){
       nconf.file({ file: 'config.json.sample'});
       spookyDmx.emitter.on('initialized',function() {
