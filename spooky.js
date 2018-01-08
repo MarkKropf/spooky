6954fcc9b4e19985b61b08b88720771452f79660
Mark Kropf <markkropf@gmail.com>
2018-01-07 23:14:43 -0500

    massive update to add lifx support

--- spooky.js
+++ spooky.js
@@ -1,19 +1,42 @@
 var nconf = require('nconf');
 var SpookyOsc = require('./lib/spooky-osc.js');
 var spookyOsc = new SpookyOsc();
-var SpookyDmx = require('./lib/spooky-dmx.js');
-var spookyDmx = new SpookyDmx();
 var spookyFixtures = require('./lib/spooky-fixtures.js');
 
 nconf.argv().env().file({ file: 'config.json'});
 
-spookyDmx.host = nconf.get('dmxGateway');
-spookyDmx.on('initialized', function() {
-  console.log("[spooky] [dmx] Initialized");
-  spookyFixtures.initialize(nconf.get('fixtures'),nconf.get('groups'),spookyDmx);
+// Parse fixtures & Load related protocols
+nconf.get("fixtures").forEach(function(value) {
+  if (value.commType === "dmx") {
+    nconf.set('dmx','true');
+  } else if (value.commType === "lifx") {
+    nconf.set('lifx','true');
+  }
 });
-spookyDmx.on('updated', function() { spookyDmx.emitDmx();
-});
-spookyDmx.on('emitted', function() {  });
-spookyDmx.initialize(nconf.get("fixtures"));
+
+// Configure Needed protocols
+if (nconf.get('lifx')=='true') {
+  var SpookyLifx = require('./lib/protocols/lifx.js');
+  var spookyLifx = new SpookyLifx();
+  spookyLifx.on('initialized', function() {
+    console.log("[spooky] [LIFX] Protocol initialized");
+  });
+  spookyLifx.initialize(nconf);
+}
+if (nconf.get('dmx')=='true') {
+  var SpookyDmx = require('./lib/spooky-dmx.js');
+  var spookyDmx = new SpookyDmx();
+  spookyDmx.host = nconf.get('dmxGateway');
+  spookyDmx.on('initialized', function() {
+    console.log("[spooky] [dmx] Initialized");
+    // need to break this out so each protocol can initialize its fixtures separately
+    spookyFixtures.initialize(nconf.get('fixtures'),nconf.get('groups'),spookyDmx,spookyLifx);
+  });
+  spookyDmx.on('updated', function() { spookyDmx.emitDmx(); });
+  spookyDmx.on('emitted', function() {  });
+  spookyDmx.initialize(nconf.get("fixtures"));
+}
+
+
+// Begin listening for OSC requests
 spookyOsc.initialize(nconf.get('host'), nconf.get('serverPort'),spookyFixtures);
